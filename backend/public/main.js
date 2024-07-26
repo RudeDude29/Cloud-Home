da69f02625fe6a86e553c25d222c68ace55336e4
RudeDude29 <godaraashish029@gmail.com>
2024-07-26 18:28:25 +0530

    F

--- backend/public/main.js
+++ backend/public/main.js
@@ -1,16 +1,17 @@
 require("dotenv").config();
-require("./config/db.js");
-const authRouter = require("./routes/authRoutes.js");
-const otpRouter = require("./routes/otpRoutes.js");
+require("../config/db.js");
+const authRouter = require("../routes/authRoutes.js");
+const otpRouter = require("../routes/otpRoutes.js");
 const express = require("express");
 const cors = require("cors");
-const verifyToken = require("./middlewares/verifyToken.js");
-const folderRouter = require("./routes/folderRoutes.js")
-const fileRouter = require("./routes/fileRoutes.js");
+const verifyToken = require("../middlewares/verifyToken.js");
+const folderRouter = require("../routes/folderRoutes.js")
+const fileRouter = require("../routes/fileRoutes.js");
+const path = require("path");
 const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
     res.send("App is running...");
 });
